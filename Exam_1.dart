deed7304ca60bb8da326dc45db8b52cdba415f24
Afsana-Akter42 <ratryafsana@gmail.com>
2025-02-12 20:46:19 +0600

    Add files via upload

---
+++ Exam_1.dart
@@ -0,0 +1,16 @@
+void triangle() {
+
+  double base = 10.0;
+  double height = 20.0;
+
+  double area = 0.5 * base * height ;
+
+  print("Triangle Area is: ${area.toStringAsFixed(2)}");
+}
+
+void main() {
+  triangle();
+
+}
+
+
